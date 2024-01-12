9f539992f3389119b7a73b62214fdc455f40d1b1
Joao Neto <jbfaneto@gmail.com>
2024-01-12 10:15:45 -0300

    LOG info added to the main class

--- src/main/java/com/joaoneto/parkinglot/ParkingLotApplication.java
+++ src/main/java/com/joaoneto/parkinglot/ParkingLotApplication.java
@@ -1,13 +1,19 @@
 package com.joaoneto.parkinglot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParkingLotApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ParkingLotApplication.class);
+
 	public static void main(String[] args) {
+		LOG.info("Starting application");
 		SpringApplication.run(ParkingLotApplication.class, args);
+		LOG.info("Application started");
 	}
 
 }
