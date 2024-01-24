0c527e43c114cfdbe515373230a1f768c2289073
Joao Neto <jbfaneto@gmail.com>
2024-01-24 20:56:01 -0300

    New feat swagger added

--- src/main/java/com/joaoneto/parkinglot/config/SpringDocOpenApiConfig.java
+++ src/main/java/com/joaoneto/parkinglot/config/SpringDocOpenApiConfig.java
@@ -1,4 +1,33 @@
 package com.joaoneto.parkinglot.config;
 
-public class SpringDoc {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Parking Lot Rest API")
+                                .description("API To manage a parking lot")
+                                .version("1.0.0")
+                                .license(
+                                        new License()
+                                                .name("MIT License")
+                                                .url("https://opensource.org/licenses/MIT")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("João Neto")
+                                                .email("jbfaneto@gmail.com")
+                                )
+                );
+    }
 }
