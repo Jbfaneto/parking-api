37b556b6be560244e27ca0df41a9e8cd9bdf5bed
Joao Neto <jbfaneto@gmail.com>
2024-02-15 18:23:21 -0300

    finished all the tests

--- src/test/java/com/joaoneto/parkinglot/UserIT.java
+++ src/test/java/com/joaoneto/parkinglot/UserIT.java
@@ -1,11 +1,12 @@
 package com.joaoneto.parkinglot;
 
 
-import com.joaoneto.parkinglot.web.dtos.UserCreateRequestDto;
-import com.joaoneto.parkinglot.web.dtos.UserCreateResponseDto;
+import com.joaoneto.parkinglot.web.dtos.*;
+import com.joaoneto.parkinglot.web.exceptions.exceptionBody.ExceptionResponseBody;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -33,7 +34,157 @@ public class UserIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.username()).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.username()).isEqualTo("tody@gmail.com");
         org.assertj.core.api.Assertions.assertThat(responseBody.role()).isEqualTo("client");
-
     }
 
+    @Test
+    public void createUserInvalidUsername() {
+        ExceptionResponseBody  responseBody = testClient.post()
+                .uri("api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateRequestDto("todygmail.com", "123456"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(400);
+    }
+
+    @Test
+    public void createUserNullUsername() {
+        ExceptionResponseBody  responseBody = testClient.post()
+                .uri("api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateRequestDto("", "123456"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(400);
+    }
+
+    @Test
+    public void createUserAlreadyExists() {
+        ExceptionResponseBody  responseBody = testClient.post()
+                .uri("api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateRequestDto("nome2@email.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.CONFLICT)
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(409);
+    }
+
+    @Test
+    public void createUserInvalidPassword() {
+        ExceptionResponseBody  responseBody = testClient.post()
+                .uri("api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateRequestDto("todygmail.com", ""))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(400);
+    }
+
+    @Test
+    public void createUserInvalidPasswordLessThanSixCharacters() {
+        ExceptionResponseBody  responseBody = testClient.post()
+                .uri("api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateRequestDto("todygmail.com", "1234"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(400);
+    }
+
+    @Test
+    public void findUserByIdWithSuccessStatus200(){
+        GetUserDto responseBody = testClient.get()
+                .uri("api/v1/users/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GetUserDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.id()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.username()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.username()).isEqualTo("admin@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.role()).isEqualTo("ADMIN");
+    }
+
+    @Test
+    public void findUserByIdNotFoundStatus404(){
+        ExceptionResponseBody responseBody = testClient.get()
+                .uri("api/v1/users/100")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(404);
+    }
+
+    @Test
+    public void updatePasswordWithSuccessStatus200() {
+        UpdateUserResponseDto responseBody = testClient.put()
+                .uri("api/v1/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UpdateUserRequestDto("123456789", "1234567", "1234567"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UpdateUserResponseDto.class)
+                .returnResult().getResponseBody();
+
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.id()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.username()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.password()).isEqualTo("1234567");
+    }
+
+    @Test
+    public void updatePasswordNotFoundStatus404() {
+        ExceptionResponseBody responseBody = testClient.put()
+                .uri("api/v1/users/100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UpdateUserRequestDto("123456789", "1234567", "1234567"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(404);
+    }
+
+    @Test
+    public void updatePasswordInvalidPasswordConfirmationStatus400() {
+        ExceptionResponseBody responseBody = testClient.put()
+                .uri("api/v1/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UpdateUserRequestDto("123456789", "1234567", "12345678"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ExceptionResponseBody.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.status()).isEqualTo(400);
+    }
 }
