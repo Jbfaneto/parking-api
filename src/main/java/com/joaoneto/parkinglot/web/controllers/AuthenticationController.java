000c42dbb7e2a635e8ba45a9c0f7b96d74bc5ec5
Joao Neto <jbfaneto@gmail.com>
2024-04-19 09:01:41 -0300

    last commit, api ready to be used

--- src/main/java/com/joaoneto/parkinglot/web/controllers/AuthenticationController.java
+++ src/main/java/com/joaoneto/parkinglot/web/controllers/AuthenticationController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
